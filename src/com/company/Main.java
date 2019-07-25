a3f1ed53c24fbe30980c28abca4d54cd3bf7a23d
vudinhdai <vudinhdai1999hnue@gmail.com>
2019-07-25 22:26:58 +0700

    hien thi crush

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name crush:");
+        String name = scanner.nextLine();
+        System.out.println("I LOVE  " + name);
+    }
+}
